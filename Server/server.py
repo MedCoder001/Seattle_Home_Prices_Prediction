2fa4c41c53161d344c02c85315d02de814734128
MedCoder001 <ibraheembashir001@gmail.com>
2024-02-14 09:18:04 +0100

    Latest Commit

--- Server/server.py
+++ Server/server.py
@@ -12,6 +12,21 @@ def get_zipcodes():
 
     return response
 
+@app.route('/predict_home_price', methods=['POST'])
+def predict_home_price():
+    beds = int(request.form['beds'])
+    baths = int(request.form['baths'])
+    size = int(request.form['size'])
+    lot_size = int(request.form['lot_size'])
+    zip_code = request.form['zip_code']
+
+    response = jsonify({
+        'estimated_price': util.get_estimated_price(beds, baths, size, lot_size, zip_code)
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response 
+
 if __name__ == '__main__':
     print("Server is running for Seattle home price prediction")
     app.run()
--- Server/util.py
+++ Server/util.py
@@ -1,9 +1,28 @@
 import json
 import pickle
+import numpy as np
 
 __zip_code = None
 __data_columns = None
 __model = None
+
+def get_estimated_price(beds, baths, size, lot_size, zip_code):
+    try:
+        zip_code_index = __data_columns.index(zip_code.lower())
+    except:
+        zip_code_index = -1
+
+    x = np.zeros(len(__data_columns))
+    x[0] = beds
+    x[1] = baths
+    x[2] = size
+    x[3] = lot_size
+    if zip_code_index >= 0:
+        x[zip_code_index] = 1
+
+    predicted_price = __model.predict([x])[0]
+    rounded_price = predicted_price.round(2)
+    return rounded_price
 
 
 def get_zipcodes():
@@ -18,6 +37,7 @@ def load_saved_artifacts():
         __data_columns = json.load(f)['data_columns']
         __zip_code = __data_columns[4:]
 
+    global __model
     with open("./Server/artifacts/Seattle_Home_Prices_Prediction_Model.pickle", "rb") as f:
         __model = pickle.load(f)
     print("loading saved artifacts...done")
@@ -25,3 +45,6 @@ def load_saved_artifacts():
 if __name__ == '__main__':
     load_saved_artifacts()
     print(get_zipcodes())
+    print(get_estimated_price(3, 2, 2000, 5000, '98126'))
+    print(get_estimated_price(2, 2, 4000, 5500, '98146'))
+    print(get_estimated_price(3, 4, 6000, 5500, '98103'))
