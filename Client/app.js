2591bc0c391a733831f3918d7d8a444eebbafb38
MedCoder001 <ibraheembashir001@gmail.com>
2024-03-02 08:39:21 +0100

    Latest Commit

--- Client/app.js
+++ Client/app.js
@@ -20,21 +20,22 @@ function getBedroomsValue() {
 
 function onClickedEstimatePrice() {
     console.log("Estimate price button clicked");
-    var size = document.getElementById("uiSizeSqft").value; // Corrected variable name
-    var lot_size = document.getElementById("uiLotSqft").value; // Corrected variable name
-    var beds = getBedroomsValue(); // Corrected function call
-    var baths = getBathroomsValue(); // Corrected function call
-    var zipcodes = document.getElementById("uiZipcodes").value; // Corrected variable name
+    var size = document.getElementById("uiSizeSqft").value; 
+    var lot_size = document.getElementById("uiLotSqft").value; 
+    var beds = getBedroomsValue(); 
+    var baths = getBathroomsValue(); 
+    var zipcodes = document.getElementById("uiZipcodes").value; 
     var estPrice = document.getElementById("uiEstimatedPrice");
 
-    var url = "http://127.0.0.1:5000/predict_home_price";
-
+    // var url = "http://127.0.0.1:5000/predict_home_price";
+    var url = "/api/predict_home_price"; // because we are using nginx as reverse proxy
+    
     $.post(url, {
         beds: beds,
         baths: baths,
         size: parseFloat(size),
         lot_size: parseFloat(lot_size),
-        zip_code: zipcodes // Corrected parameter name
+        zip_code: zipcodes 
     }, function(data, status) {
         console.log(data.estimated_price);
         estPrice.innerHTML = "<h2>" + data.estimated_price.toString() + " USD</h2>";
@@ -44,7 +45,8 @@ function onClickedEstimatePrice() {
 
 function onPageLoad() {
     console.log("document loaded");
-    var url = "http://127.0.0.1:5000/get_zipcodes";
+    // var url = "http://127.0.0.1:5000/get_zipcodes";
+    var url = "/api/get_zipcodes"; // because we are using nginx as reverse proxy
     $.get(url, function(data, status) {
         console.log("got response for get_zipcodes request");
         if(data && data.zipcodes) {
